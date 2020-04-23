555347043dd6a03719d9e1139519f109b98cc65d
ThienNguyen <doithua9669@gmail.com>
2020-04-23 21:18:35 +0700

    part 1

---
+++ tempData.js
@@ -0,0 +1,64 @@
+export default tempData = [{
+    name: "Plan a trip",
+    color: "#24A6D9",
+    todos: [{
+            title: "Book flight",
+            completed: false
+        },
+        {
+            title: "Passport check",
+            completed: true
+        },
+        {
+            title: "Reserve Hotel room",
+            completed: true
+        },
+        {
+            title: "Pack Luggage",
+            completed: false
+        }
+    ]
+},
+{
+    name: "Birth day party",
+    color: "#595BD9",
+    todos: [{
+            title: "Get Balloons",
+            completed: false
+        },
+        {
+            title: "Send Invitations",
+            completed: true
+        },
+        {
+            title: "Make dinner reservations",
+            completed: true
+        },
+        {
+            title: "Pack Luggage",
+            completed: true
+        }
+    ]
+},
+{
+    name: "Learn React JS",
+    color: "#24A6D9",
+    todos: [{
+            title: "Get Document",
+            completed: true
+        },
+        {
+            title: "Prepare Computer",
+            completed: true
+        },
+        {
+            title: "Some Foods",
+            completed: true
+        },
+        {
+            title: "Ready learn",
+            completed: false
+        }
+    ]
+}
+];
